947cf18839017ebf3ffbb9adeeb8ca0154d25b45
Shlomo-kodkod <quarts.91.elders@icloud.com>
2025-09-01 14:22:33 +0300

    feat: add processor class

---
+++ app/processor.py
@@ -0,0 +1,40 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import logging
+
+
+class Processor:
+    @staticmethod
+    def calculate_sentiment_score(text: str) -> str:
+        """
+        Analyzes the sentiment of a given text string and returns a sentiment score.
+        """
+        score= SentimentIntensityAnalyzer().polarity_scores(text)
+        result = score["compound"]
+        logging.info("Successfully calculated sentiment score")
+        if result >= 0.5: return "positive"
+        elif -0.49 < result <= 0.49: return "negative"
+        else: return "neutral"
+
+    
+    @staticmethod
+    def find_weapons(text: str, weapons: list) -> list | None:
+        """
+        Find weapons in the given text using the provided set of weapons.
+        """
+        found_weapons = [weapon for weapon in weapons if weapon in text]
+        if found_weapons:
+            logging.info("Successfully found weapons")
+            return found_weapons
+        else:
+            logging.info("Weapons not found")
+            return None
+    
+        
+
+    
+
+        
+    
+
+    
